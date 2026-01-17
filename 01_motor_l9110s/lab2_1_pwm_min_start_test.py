80c16ea1a9a14abee8cd283e980a2f5396788e78
shinejihun1227 <yeom122727@gmail.com>
2026-01-17 18:24:46 +0900

    Create lab2_1_pwm_min_start_test.py

---
+++ 01_motor_l9110s/lab2_1_pwm_min_start_test.py
@@ -0,0 +1,15 @@
+from machine import Pin, PWM
+import time
+
+IA = PWM(Pin(25), freq=20000)
+IB = PWM(Pin(26), freq=20000)
+
+def forward(s):
+    IB.duty(0); IA.duty(s)
+
+print("0~1023 올리면서 '처음 움직이는 값' 찾기")
+for s in range(0, 1024, 50):
+    forward(s)
+    print("duty =", s)
+    time.sleep(0.6)
+
