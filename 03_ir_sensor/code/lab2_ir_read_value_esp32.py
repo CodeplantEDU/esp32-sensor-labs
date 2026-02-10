29e9801f69994beadcda6b701c5deba6298335bd
CodeplantEDU <echoglow.official@gmail.com>
2026-02-10 11:51:46 +0900

    Create lab2_ir_read_value_esp32.py

---
+++ 03_ir_sensor/code/lab2_ir_read_value_esp32.py
@@ -0,0 +1,27 @@
+# [실습1] IR 센서 아날로그 값 읽기
+# 배선: AO -> GPIO26, VCC -> 3V3, GND -> GND
+
+from machine import Pin, ADC
+import time
+
+IR_PIN = 26
+adc = ADC(Pin(IR_PIN))
+
+# ESP32 ADC 설정 (권장)
+try:
+    adc.atten(ADC.ATTN_11DB)     # 0 ~ 약 3.3V 측정
+    adc.width(ADC.WIDTH_12BIT)   # 12bit 해상도 (0~4095)
+except:
+    pass
+
+print("[실습1] IR 아날로그 값 읽기 시작")
+print("범위: 0 ~ 4095 (값이 클수록 반사 강함)")
+
+while True:
+    try:
+        val = adc.read()        # MicroPython ESP32 (0~4095)
+    except:
+        val = adc.read_u16()    # 일부 보드 (0~65535)
+
+    print("IR =", val)
+    time.sleep(0.1)
